aeecf5e73f20104dc937832eef0a37ce4c7aaf87
Seonghyeon Kim <self@seonghyeon.dev>
2020-04-18 19:43:25 +0900

    fsm으로 정확성 보장하기

--- Program.fs
+++ Program.fs
@@ -437,6 +437,112 @@ let usd10 = 10.0<USD>
 //gbp10 + 1.0               // not allowed: didn't specify a currency
 gbp10 + 1.0<_>            // allowed using wildcard
 
+// Worked example: Designing for correctness (with FSM)
+
+type CartItem = string
+type EmptyState = NoItems
+type ActiveState = {UnpaidItems: CartItem list;}
+type PaidForState = {PaidItems: CartItem list; Payment: decimal}
+
+type Cart =
+    | Empty of EmptyState
+    | Active of ActiveState
+    | PaidFor of PaidForState
+    
+let addToEmptyState item =
+    Cart.Active {UnpaidItems=[item]}
+
+let addToActiveState state itemToAdd =
+    let newList = itemToAdd :: state.UnpaidItems
+    Cart.Active { state with UnpaidItems=newList }
+
+let removeFromActiveState state itemToRemove =
+    let newList = state.UnpaidItems |> List.filter(fun i -> i <> itemToRemove)
+    
+    match newList with
+        | [] -> Cart.Empty NoItems
+        | _ -> Cart.Active {state with UnpaidItems=newList}
+
+let payForActiveState state amount =
+    Cart.PaidFor { PaidItems=state.UnpaidItems; Payment=amount }
+    
+type EmptyState with
+    member this.Add = addToEmptyState
+
+type ActiveState with
+    member this.Add = addToActiveState this
+    member this.Remove = removeFromActiveState this
+    member this.Pay = payForActiveState this
+
+let addItemToCart cart item =
+    match cart with
+        | Empty state -> state.Add item
+        | Active state -> state.Add item
+        | PaidFor _ ->
+            printfn "Error: The cart if paid for"
+            cart
+
+let removeItemFromCart cart item =
+    match cart with
+        | Empty state ->
+            printfn "Error: The cart is empty"
+            cart
+        | Active state -> state.Remove item
+        | PaidFor state ->
+            printfn "Error: The cart is paid for"
+            cart
+        
+let displayCart cart =
+    match cart with
+        | Empty _ ->
+            printfn "The cart is empty"
+        | Active state ->
+            printfn "The cart contains %A unpaid items" state.UnpaidItems
+        | PaidFor state ->
+            printfn "The cart contains %A paid items. Amount paid: %f" state.PaidItems state.Payment
+
+type Cart with
+    static member NewCart = Cart.Empty NoItems
+    member this.Add = addItemToCart this
+    member this.Remove = removeItemFromCart this
+    member this.Display = displayCart this
+    
+let emptyCart = Cart.NewCart
+printf "emptyCart = " ; emptyCart.Display
+
+let cartA = emptyCart.Add "A"
+printf "cartA = " ; cartA.Display
+
+let cartAB = cartA.Add "B"
+printf "cartAB = " ; cartAB.Display
+
+let cartB = cartAB.Remove "A"
+printf "cartB = " ; cartB.Display
+
+let emptyCart2 = cartB.Remove "B"
+printf "emptyCart2 = " ; emptyCart2.Display
+
+let emptyCart3 = emptyCart2.Remove "B"
+printf "emptyCart3="; emptyCart3.Display
+
+
+let emptyCartPaid =
+    match emptyCart with
+        | Empty _ | PaidFor _ -> emptyCart
+        | Active state -> state.Pay 100m
+printf "emptyCartPaid = " ; emptyCartPaid.Display
+
+let cartABPaid = 
+    match cartAB with
+    | Empty _ | PaidFor _ -> cartAB
+    | Active state -> state.Pay 100m
+printf "cartABPaid = " ; cartABPaid.Display
+
+let cartABPaidAgain = 
+    match cartABPaid with
+    | Empty _ | PaidFor _ -> cartABPaid
+    | Active state -> state.Pay 100m
+printf "cartABPaidAgain = " ; cartABPaidAgain.Display
 
 [<EntryPoint>]
 let main argv =
